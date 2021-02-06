7d1865a95b45f043bc8f8078663e872b055d4e80
Tassilo <tassia710@gmail.com>
2021-02-07 00:59:19 +0100

    Update EventTest.java

--- src/test/java/net/tassia/event/EventTest.java
+++ src/test/java/net/tassia/event/EventTest.java
@@ -9,6 +9,8 @@ public class EventTest {
 	@Test
 	public void test() {
 		EventManager manager = new EventManager();
+		manager.registerEvent(TestEvent.class);
+
 		manager.registerListener(TestEvent.class, new TestListener());
 		manager.registerListener(TestEvent.class, this::testListener);
 
